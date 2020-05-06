b3b495e2938f07184e48bf50418b7feaf5ac193d
bblezek <Brin.Blezek@trojans.dsu.edu>
2020-05-06 10:25:39 -0500

    Added more metrics and a confusion matrix to view results more accurately and clearly

--- Main.py
+++ Main.py
@@ -14,6 +14,9 @@ import joblib
 import cv2
 import tqdm
 import random
+import sklearn
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
 
 fruit_folder = pathlib.Path('D:/Brin/fruit-recognition/')
 # Creating list of all image file names
@@ -45,7 +48,7 @@ def prepare_image_and_label(filepath):
 
     # Then reading and resizing the image
     image = cv2.imread(fp)
-    image = cv2.resize(image, dsize=(258, 320), interpolation=cv2.INTER_CUBIC)
+    image = cv2.resize(image, dsize=(258, 320), interpolation=cv2.INTER_AREA)
     return image, label
 
 # Create dataset
@@ -77,14 +80,18 @@ else:
 images = np.asarray(images)
 labels = np.asarray(labels)
 
+batch_size=32
+
 # Splitting dataset into testing and training sets
 test_size = int(0.15 * image_count)
+if test_size%batch_size != 0:
+    test_size = test_size - (test_size%batch_size)
 test_images = images[:test_size]
 train_images = images[test_size:]
 test_labels = labels[:test_size]
 train_labels = labels[test_size:]
 
-model = models.Sequential()
+model = tf.keras.models.Sequential()
 model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(320, 258, 3)))
 model.add(layers.MaxPooling2D((2, 2)))
 
@@ -109,16 +116,25 @@ model.summary()
 epochs = 25
 learning_rate = 0.00001
 decay = learning_rate/epochs
+metrics = [
+    tf.keras.metrics.TruePositives(name='tp'),
+    tf.keras.metrics.FalsePositives(name='fp'),
+    tf.keras.metrics.TrueNegatives(name='tn'),
+    tf.keras.metrics.FalseNegatives(name='fn'),
+    tf.keras.metrics.BinaryAccuracy(name='accuracy'),
+    tf.keras.metrics.Precision(name='precision'),
+    tf.keras.metrics.Recall(name='recall'),
+    tf.keras.metrics.AUC(name='auc')
+]
 
 model.compile(loss='categorical_crossentropy',
               optimizer=optimizers.SGD(learning_rate=learning_rate, momentum=0.99),
-              metrics=['accuracy'])
-
+              metrics=metrics)
 
 history = model.fit(train_images,
                     train_labels,
                     epochs=epochs,
-                    batch_size=32,
+                    batch_size=batch_size,
                     validation_split=0.15,
                     shuffle=True
                     )
@@ -130,7 +146,7 @@ val_loss = history.history['val_loss']
 
 epochs = range(1, len(acc) + 1)
 
-#Plotting results
+#Plotting results of training
 plt.plot(epochs, acc, 'bo', label='Training acc')
 plt.plot(epochs, val_acc, 'b', label='Validation acc')
 plt.legend()
@@ -142,8 +158,39 @@ plt.title('Training and validation loss')
 plt.legend()
 plt.show()
 
-model.evaluate(test_images,
-               test_labels)
+test_steps = int(test_size/batch_size)
 
+# model.predict won't accept uint8 numpy arrays, so casting to float32
+test_images_float = np.float32(test_images)
 
+# Making predictions about test set
+test_predictions = model.predict(test_images_float,
+                                 batch_size=batch_size,
+                                 steps=test_steps)
 
+# Using model to evaluate test images
+test_results = model.evaluate(test_images,
+                              test_labels,
+                              batch_size=batch_size,
+                              verbose=0)
+
+# Printing metric results for test images from evaluating model
+for name, value in zip(model.metrics_names, test_results):
+    print(name, ' : ', value)
+print()
+
+# Converting one-hot encoded labels to a number for confusion matrix
+int_test_predictions = np.argmax(test_predictions, axis = 1)
+int_test_labels = np.argmax(test_labels, axis=1)
+
+# Mapping true test results versus what they were predicted to be
+cm = confusion_matrix(int_test_labels,int_test_predictions, normalize='true')
+plt.figure(figsize=(12,12))
+sns.heatmap(cm, annot=True, fmt=".2f")
+tick_marks = np.arange(len(directories))
+plt.xticks(tick_marks, directories, rotation=45)
+plt.yticks(tick_marks, directories, rotation=45)
+plt.ylabel('Actual label')
+plt.xlabel('Predicted label')
+plt.show()
+plt.savefig('Confusion_Matrix.png')
